d36e85c7bbd7a77ed715da1c8ae25f488829f224
ajithshan03 <ajithshan03@gmail.com>
2017-11-21 12:06:49 +0530

    Ex-1

---
+++ Ex-1/operators/script.js
@@ -0,0 +1,111 @@
+//Lecture: variable
+/*
+var name = 'ajith';
+console.log(name);
+
+var lastName = 'shan'
+console.log(lastName);
+
+var age = 19;
+console.log(age);
+
+var fullAge = true;
+console.log(fullAge);
+*/
+
+//Lectuer:Variable 2
+/*
+var name = 'ajith';
+var age = 19;
+//console.log(name + age);
+//console.log(age + age);
+var job,isMarried;
+//console.log(job);
+
+job = 'Engineer';
+isMarried = false;
+//console.log(name + ' is a ' + age + ' years old ' + job + '.'+ 'Is he is married?' + isMarried + '.');
+
+job = 'Driver';
+age = 'Nineteen';
+
+//console.log(name + ' is a ' + age + ' years old ' + job + '.'+ 'Is he is married?' + isMarried + '.');
+
+//var lastName = prompt('what is your last name');
+//console.log(lastName);
+
+alert (name + ' is a ' + age + ' years old ' + job + '.'+ 'Is he is married?' + isMarried + '.');
+
+*/
+//Lecture : Operator
+var ageOfAjith = 0;
+var ageOfPrabhu = 13;
+
+console.log("Calculating the difference of their age!");
+
+ageOfAjith = 20;
+ageOfPrabhu = 33;
+
+var theDifference = ageOfPrabhu - ageOfAjith;
+console.log(theDifference);
+
+ageOfAjith++;
+ageOfPrabhu++;
+
+console.log("ageOfAjith and ageOfPrabhu after one year of calculation is!");
+console.log(ageOfAjith,ageOfPrabhu);
+
+console.log("ageOfAjith and ageOfPrabhu after 5times the year of calculation is!");
+
+ageOfAjith = ageOfAjith * 5;
+ageOfPrabhu = ageOfPrabhu * 5;
+
+console.log(ageOfAjith,ageOfPrabhu);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
