07d800780ee3873aec1b129e39d6f5bb85b31143
ajithshan03 <ajithshan03@gmail.com>
2017-11-21 09:00:55 +0530

    which says about variables and data types

---
+++ Ex-1/script.js
@@ -0,0 +1,38 @@
+//Lecture: variable
+/*
+var name = 'ajith';
+console.log(name);
+
+var lastName = 'shan'
+console.log(lastName);
+
+var age = 19;
+console.log(age);
+
+var fullAge = true;
+console.log(fullAge);
+*/
+
+//Lectuer:Variable 2
+
+var name = 'ajith';
+var age = 19;
+//console.log(name + age);
+//console.log(age + age);
+var job,isMarried;
+//console.log(job);
+
+job = 'Engineer';
+isMarried = false;
+//console.log(name + ' is a ' + age + ' years old ' + job + '.'+ 'Is he is married?' + isMarried + '.');
+
+job = 'Driver';
+age = 'Nineteen';
+
+//console.log(name + ' is a ' + age + ' years old ' + job + '.'+ 'Is he is married?' + isMarried + '.');
+
+//var lastName = prompt('what is your last name');
+//console.log(lastName);
+
+alert (name + ' is a ' + age + ' years old ' + job + '.'+ 'Is he is married?' + isMarried + '.');
+
