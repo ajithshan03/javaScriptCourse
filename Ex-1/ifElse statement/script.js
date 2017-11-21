83789483853141020699f0bb507e8411ac2ab42b
ajithshan03 <ajithshan03@gmail.com>
2017-11-21 14:58:56 +0530

    if/else statement program

---
+++ Ex-1/ifElse statement/script.js
@@ -0,0 +1,61 @@
+//Lecture: If/Else Statement
+var name = 'ajith';
+var isMarried = false;
+
+if (isMarried === true) {
+    console.log(name + ' is married :)');
+} else {
+    console.log(name + ' is hopeflly married soon!');
+   
+}
+// comparision with ==
+isMarried = true;
+var familyDetail = 0;
+console.log('Is ' + name + ' has child?');
+if(familyDetail == false) {
+    console.log(name + ' has no child');
+} else {
+    console.log(name + ' has child');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
